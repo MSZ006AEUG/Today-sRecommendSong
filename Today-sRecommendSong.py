fa41c5b026ecef2c3497e235f5af671a9ee12834
MSZ006AEUG <gundam00solidsnake@yahoo.co.jp>
2017-05-06 22:01:14 +0900

    json

--- Today-sRecommendSong.py
+++ Today-sRecommendSong.py
@@ -1,10 +1,10 @@
-import sys, random, urllib.request, json, requests
+import sys, random, urllib.request, json, requests, urllib.parse
 import io
 from lxml import etree
 sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 
 #Read XML
-xml_path = "C:/Users/ZETA/Music/iTunes/iTunes Music Library.xml"
+xml_path = "C:/Users/znokodo/Music/iTunes/iTunes Music Library.xml"
 tree = etree.parse(xml_path)
 root = tree.getroot()
 
@@ -36,21 +36,32 @@ SongYear = TodaysSong["Year"]
 
 
 #Get Artwork
-'''JsonURL = "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/wa/wsSearch?"
-JsonURL = JsonURL + "term=" + SongTitle + "&term=" + ArtistName  + "&term=" + AlbumTitle  + "&country=JP&entity=musicTrack"
+JsonURL = "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/wa/wsSearch?"
+SearchKey = SongTitle + " " + ArtistName + " " + AlbumTitle
+param = {
+    "term": SearchKey,
+    "country": "JP",
+    "entity": "musicTrack",
+}
+paramStr = urllib.parse.urlencode(param)
+#print(paramStr)
+readObj = urllib.request.urlopen(JsonURL + paramStr)
+response = readObj.read()
 
-response = urllib.request.urlopen(JsonURL)
-content = json.loads(response.read().decode("utf-8"))
+content = json.loads(response.decode("utf-8"))
 print(content)
-print(":{}".format(content['collectionViewUrl']))
-iTunesURL = ":{}".format(content['collectionViewUrl'])'''
+Json_content = json.dumps(content)
+print(type(Json_content))
+iTunesURL = content["collectionViewUrl"]
+
+print(iTunesURL)
 
 
 #Post to Slack
-PostText = "本日の1曲\n" + SongTitle + " / " + ArtistName + " (" + SongYear + ") from " + AlbumTitle
+'''PostText = "本日の1曲\n" + SongTitle + " / " + ArtistName + " (" + SongYear + ") from " + AlbumTitle + "\n" + iTunesURL
 requests.post("https://hooks.slack.com/services/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", data = json.dumps({
     "text": PostText,
     "username": u"Today's Recommend Song",
     "icon_emoji": u":psychedelic:",
     "link_names": 1,
-}))
+}))'''
